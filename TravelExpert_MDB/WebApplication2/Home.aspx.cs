066e1e49e73bfcd43a9ecbeb816dd57f97b4055b
ACDM\615786 <haishui7698@gmail.com>
2018-01-28 19:12:05 -0700

    TravelExpert

---
+++ TravelExpert_MDB/WebApplication2/Home.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{  /**
+     ** purpose:display homepage
+     ** Author:Timothy 
+     ** Date:Jan 21,2018
+    **/
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
